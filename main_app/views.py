a4d03f25e941ea9ef9d8193d8c0e436a368c21e4
larrytu327 <larrytu327@gmail.com>
2023-06-20 20:59:44 -0700

    setup views and url.py for project and app

--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.views import View
+from django.http import HttpResponse
 
+class Home(View):
+    def get(self, request):
+        return HttpResponse("Card Collector Home")
 # Create your views here.
