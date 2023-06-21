979ab18a984c456f812e2296c9e783c694219784
larrytu327 <larrytu327@gmail.com>
2023-06-20 22:43:26 -0700

    added temporary hardcoded database

--- main_app/urls.py
+++ main_app/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.Home.as_view(), name="home"), 
+    path('cards/', views.CardsList.as_view(), name="cards_list"),
+    path('about/', views.About.as_view(), name="about"),
 ]
