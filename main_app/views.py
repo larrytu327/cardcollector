fd678108fa4fa204fb74ab4289c0d6731117f30c
larrytu327 <larrytu327@gmail.com>
2023-06-20 21:54:17 -0700

    added template inheritance and static files, scripts and styles

--- main_app/views.py
+++ main_app/views.py
@@ -1,8 +1,7 @@
 from django.shortcuts import render
 from django.views import View
 from django.http import HttpResponse
+from django.views.generic.base import TemplateView
 
-class Home(View):
-    def get(self, request):
-        return HttpResponse("Card Collector Home")
-# Create your views here.
+class Home(TemplateView):
+    template_name = "home.html"
